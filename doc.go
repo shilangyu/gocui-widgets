3680275cfe9109987f1efde19d6ccdbe30d10f23
shilangyu <xmarcinmarcin@gmail.com>
2019-05-26 17:13:39 +0200

    initial godoc

---
+++ doc.go
@@ -0,0 +1,28 @@
+/*
+Package widgets gives you widgets that allow you to quickly start writing TUI apps with gocui.
+Creating a widget is easy:
+	package main
+
+	import (
+		"github.com/jroimartin/gocui"
+		widgets "github.com/shilangyu/gocui-widgets"
+	)
+
+	func main() {
+		g, err := gocui.NewGui(gocui.OutputNormal)
+		if err != nil {
+			panic(err)
+		}
+		defer g.Close()
+
+		w, h := g.Size()
+		text := widgets.NewText("example-widget", "hello world", true, true, w/2, h/2)
+
+		g.SetManager(text)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		panic(err)
+	}
+*/
+package widgets
