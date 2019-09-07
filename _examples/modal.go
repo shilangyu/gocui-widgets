bc7d158028dcea03c76063f08eab37288cf7cac5
shilangyu <xmarcinmarcin@gmail.com>
2019-09-07 16:10:01 +0200

    created example for modal

---
+++ _examples/modal.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+
+	"github.com/jroimartin/gocui"
+	widgets "github.com/shilangyu/gocui-widgets"
+)
+
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		panic(err)
+	}
+	defer g.Close()
+	g.Mouse = true
+
+	choices := []string{
+		"OK",
+		"CLOSE",
+	}
+
+	w, h := g.Size()
+	modalWi := widgets.NewModal("modal", "Hey im a pop-up.\nOk bye", choices, true, w/2, h/2, nil, func(i int) {
+		switch i {
+		case 1:
+			g.Close()
+			os.Exit(0)
+		}
+	})
+
+	g.SetManager(modalWi)
+
+	g.Update(func(g *gocui.Gui) error {
+		g.SetCurrentView("modal")
+		return nil
+	})
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		panic(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		panic(err)
+	}
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
